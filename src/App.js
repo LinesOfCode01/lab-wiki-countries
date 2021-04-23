dfc857c7d26743c0f9668c4e47dc1573172ae7a8
linesofcode01 <frcoding@outlook.com>
2021-04-22 23:53:30 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+
+      <div className="container">
+        <div className="row">
+          <CountriesList />
+
+          <Switch>
+            <Route exact path="/country/:location" component={CountryDetails} />
+          </Switch>
+        </div>
+      </div>
     </div>
   );
 }
